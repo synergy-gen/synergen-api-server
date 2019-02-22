d2ea40b1db9be98e6639956ce172402c2b72319d
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-02-22 15:51:30 -0800

    adding logging to request body validation

--- src/api/v1/validate.js
+++ src/api/v1/validate.js
@@ -1,11 +1,14 @@
 const response = require('./response');
 const status = require('http-status');
 const joi = require('joi');
+const logger = require('winstonson')(module);
 
 module.exports = (action, schema, query) => (req, res, next) => {
     let result = query ? joi.validate(req.query, schema) : joi.validate(req.body, schema);
     if (result.error) {
         let errors = result.error.details.map(d => d.message);
+        logger.error('Invalid request body. See details below.');
+        logger.error(errors);
         return response.sendErrorResponse(res, status.BAD_REQUEST, 'Failed to ' + action, errors);
     }
     next();
